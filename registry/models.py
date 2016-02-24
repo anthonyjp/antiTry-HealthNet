84d42785443c44aaca97d52add6317a74ec671c0
Matthew Crocco <matthewcrocco@gmail.com>
2016-02-24 12:24:34 -0500

    Migrate database and start model hierarchy

--- registry/models.py
+++ registry/models.py
@@ -1,44 +1,39 @@
 from django.db import models
 
-# Create your models here.
-class Patient(models.Model):
+GENDER_CHOICES = (
+    ('M', 'Male'),
+    ('F', 'Female')
+)
+
+BLOOD_CHOICES = (
+    ('A', 'A'),
+    ('B', 'B'),
+    ('AB', 'AB'),
+    ('O', 'O')
+)
+
+INSURANCE_CHOICES = (
+    ('Self Insured', 'Self Insured'),
+    ('Blue Cross Blue Shield', 'Blue Cross Blue Shield'),
+    ('Super Legit MI', 'Super Legit Medical Insurance'),
+    ('Not Legit MI', 'Not Legit Medical Insurance')
+)
+
+
+class User(models.Model):
     firstName = models.CharField(max_length=100)
     middleInitial = models.CharField(max_length=1)
     lastName = models.CharField(max_length=100)
+    email = models.EmailField()
     dateOfBirth = models.DateField()
-    MALE = 'M'
-    FEMALE = 'F'
-    GENDER_CHOICES = (
-        (MALE, 'Male'),
-        (FEMALE, 'Female')
-    )
-    gender = models.CharField(max_length=1,
-                                      choices=GENDER_CHOICES,
-                                      default=MALE)
-    email = models.EmailField(max_length=100)
+
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default=GENDER_CHOICES[0][0])
     password = models.CharField(max_length=50)
     securityAnswer = models.CharField(max_length=50)
-    height = models.IntegerField()
-    weight = models.IntegerField()
-    OTYPE = 'O'
-    ATYPE = 'A'
-    BTYPE = 'B'
-    ABTYPE = 'AB'
-    BLOOD_CHOICES = (
-        (OTYPE, 'O'),
-        (ATYPE, 'A'),
-        (BTYPE, 'B'),
-        (ABTYPE, 'AB')
-    )
-    bloodType = models.CharField(max_length=2, choices = BLOOD_CHOICES,default = OTYPE)
-    SELF = 'self'
-    BCBS = 'bcbs'
-    SLMI = 'slmi'
-    NLMI = 'nlmi'
-    INSURANCE_CHOICES = (
-        (SELF, 'Self Insured'),
-        (BCBS, 'Blue Cross Blue Shield'),
-        (SLMI, 'Super Legit Medical Insurance'),
-        (NLMI, 'Not Legit Medical Insurance')
-    )
-    insurance = models.CharField(max_length =4, choices = INSURANCE_CHOICES, default = SELF)
+
+
+class Patient(User):
+    height = models.PositiveIntegerField()
+    weight = models.PositiveIntegerField()
+    bloodType = models.CharField(max_length=2, choices=BLOOD_CHOICES, default=BLOOD_CHOICES[0][0])
+    insurance = models.CharField(max_length=40, choices=INSURANCE_CHOICES, default=INSURANCE_CHOICES[0][0])
