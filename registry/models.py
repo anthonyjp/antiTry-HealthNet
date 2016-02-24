3ecd6d7b0fe727c9cc83b88919a7903b59138240
Matthew Crocco <matthewcrocco@gmail.com>
2016-02-24 13:37:52 -0500

    Create Hospital model and utilities incl. Dictionary model and update git ignore

--- registry/models.py
+++ registry/models.py
@@ -1,4 +1,7 @@
+from localflavor.us import us_states
 from django.db import models
+
+from registry.utility.models import Dictionary
 
 GENDER_CHOICES = (
     ('M', 'Male'),
@@ -19,6 +22,35 @@ INSURANCE_CHOICES = (
     ('Not Legit MI', 'Not Legit Medical Insurance')
 )
 
+class Hospital(models.Model):
+    """
+    Represents a hospital with a location
+    """
+
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=300)
+    state = models.CharField(max_length=2, choices=us_states.STATE_CHOICES, blank=False, null=False)
+    zipcode = models.CharField(max_length=10, blank=False, null=False)
+    identifiers = models.ForeignKey(to=Dictionary, on_delete=models.SET(Dictionary.empty))
+
+    def get_location(self) -> str:
+        """
+        Returns the location of the hospital as combined location fields of the form:
+
+        <address>, <state> <zipcode>
+        :return: Location
+        """
+        return "%s, %s %s" % (self.address, self.state, self.zipcode)
+
+    # TODO get coordinates
+    def get_coordinates(self):
+        """
+        Returns the Latitutde and Longitude of the hospital, if possible, as a Tuple of two floats. If the location
+        can not be determined the Tuple contains Not-a-Number.
+
+        :return: (latitude,longitude)
+        """
+        return float('NaN'), float('NaN')
 
 class User(models.Model):
     firstName = models.CharField(max_length=100)
@@ -26,6 +58,9 @@ class User(models.Model):
     lastName = models.CharField(max_length=100)
     email = models.EmailField()
     dateOfBirth = models.DateField()
+
+    curHospital = models.ForeignKey(to=Hospital, related_name='cur_hospital', on_delete=models.PROTECT, null=False)
+    prefHospital = models.ForeignKey(to=Hospital, related_name='pref_hospital', on_delete=models.SET_NULL, null=True)
 
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default=GENDER_CHOICES[0][0])
     password = models.CharField(max_length=50)
@@ -35,5 +70,6 @@ class User(models.Model):
 class Patient(User):
     height = models.PositiveIntegerField()
     weight = models.PositiveIntegerField()
+
     bloodType = models.CharField(max_length=2, choices=BLOOD_CHOICES, default=BLOOD_CHOICES[0][0])
     insurance = models.CharField(max_length=40, choices=INSURANCE_CHOICES, default=INSURANCE_CHOICES[0][0])
---
+++ registry/utility/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Dictionary(models.Model):
+    name = models.CharField(max_length=100)
+
+    def empty(self):
+        return Dictionary.objects.create()
+
+class KeyValue(models.Model):
+    dict = models.ForeignKey(to=Dictionary, on_delete=models.CASCADE)
+    key = models.CharField(max_length=100)
+    value = models.TextField()
