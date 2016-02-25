d6630c6383e81f9a497c7191807ef3ea9e1e5099
Matthew Crocco <matthewcrocco@gmail.com>
2016-02-25 18:17:02 -0500

    Remove leading '/' from urls (it was redundant)

--- registry/urls.py
+++ registry/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^/patient/new$', views.new, name='new'),
-    url(r'^/detail/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^patient/new$', views.new, name='new'),
+    url(r'^detail/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
 ]
