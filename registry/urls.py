959857fc0387243bd58165c0720dfed6a9fac74e
Matthew Crocco <matthewcrocco@gmail.com>
2016-02-25 13:56:02 -0500

    Fix urls.py

--- registry/urls.py
+++ registry/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$/new', views.new, name='new'),
-    url(r'^(?P<pk>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^$', views.index, name='index'),
+    url(r'^/patient/new$', views.new, name='new'),
+    url(r'^/detail/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
 ]
