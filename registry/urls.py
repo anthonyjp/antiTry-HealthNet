adfbdd52aef1623a88e07f91e96c91fe085d3c4e
Anthony Perez <ajp8707@g.rit.edu>
2016-02-26 13:45:02 -0500

    Made changes to the landing page buttons to redirect through django instead of href in html. Also added the required views and urls in order for the new links to work.

--- registry/urls.py
+++ registry/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = 'registry'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^register$', views.register, name='register'),
+    url(r'^login$', views.login, name='login'),
     url(r'^patient/new$', views.new, name='new'),
     url(r'^detail/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
 ]
