7eb7778f9fac94c1a412fa08b52b8fb3e6965824
Anthony Perez <ajp8707@g.rit.edu>
2016-03-06 21:10:27 -0500

    - Changed urls and views to manage 'home' as homepage for logged users.  - Created new css and js for base_user  - Created new templates for base user, and home  - Made home tabs to display different information

--- static/registry/js/base_user.js
+++ static/registry/js/base_user.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
     //  When user clicks on tab, this code will be executed
-    $("#mtabs li").click(function() {
+    $("#tab-links li").click(function() {
         //  First remove class "active" from currently active tab
-        $("#mtabs li").removeClass('active');
+        $("#tab-links li").removeClass('active');
 
         //  Now add class "active" to the selected/clicked tab
         $(this).addClass("active");
 
         //  Hide all tab content
-        $(".mtab_content").hide();
+        $(".tab").hide();
 
         //  Here we get the href value of the selected tab
         var selected_tab = $(this).find("a").attr("href");
