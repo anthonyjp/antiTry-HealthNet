f9c7af126e8987499c49c45dded9fe62a66def8a
Kyle Scagnelli <kjs5107@rit.edu>
2016-04-14 17:51:13 -0400

    Wrote skeleton outlines for every single test function. There're a lot of them.

--- registry/tests.py
+++ registry/tests.py
@@ -53,13 +53,19 @@ def create_patient_dict(email, password, name="The L Diablo", dob=dt.datetime.no
     }
 
 
-class PatientTest(TestCase):
+class RegistrationTest(TestCase):
+    """
+    All cases involving Patient registration, Unknown Patient Registration
+    Staff Registration (Nurse or Doc)
+    """
+    # patient registration cases
     def setUp(self):
-        doctor = Doctor(first_name="Sir", last_name="No", date_of_birth=dt.date(1985, 5, 29), gender=Gender.MALE, security_question=SecQ.Q1, security_answer="YEP")
+        doctor = Doctor(first_name="Sir", last_name="No", date_of_birth=dt.date(1985, 5, 29), gender=Gender.MALE,
+                        security_question=SecQ.Q1, security_answer="YEP")
         doctor.auth_user = DjangoUser.objects.create_user(username="DoctorNo", email="doc@doc.com", password="qwerty123")
         doctor.save()
 
-    def test_patient_registration(self):
+    def test_pat_reg_pat_and_user_exist(self):
         email = "test@test.com"
         passwd = 'qwerty123'
 
@@ -72,3 +78,348 @@ class PatientTest(TestCase):
         self.assertIsInstance(pat, User, 'get_subclass patient is not also user!')
         self.assertIsNotNone(pat.auth_user.username, 'Patient username not set properly!')
 
+    ### Note: ec = Emergency Contact
+    # success cases
+    def test_pat_reg_succes_add_ec(self):
+        pass
+
+    def test_pat_reg_success_ec_dropdown(self):
+        pass
+
+    # failure cases
+    def test_pat_reg_fail_incomplete(self):
+        pass
+
+    def test_pat_reg_fail_cancel(self):
+        pass
+
+    # unknown patient registration cases
+    # success case
+    def test_unknown_pat_reg_success(self):
+        # pick case 42 or 43, doesn't matter
+        pass
+
+    # failure case
+    def test_unknown_pat_reg_fail_cancel(self):
+        pass
+
+    # staff registration cases
+    # Note: The admin registers the staff!
+    # success case
+    def test_staff_reg_success(self):
+        pass
+
+    # failure cases
+    def test_staff_reg_fail_incomplete(self):
+        pass
+
+    def test_staff_reg_fail_cancel(self):
+        pass
+
+
+class AppointmentTest(TestCase):
+    """
+    All things to do with Appointments
+    Create, update, cancel, view
+    """
+    # create tests
+    # for all these methods include success, failure due to insufficient info, cancel, and time conflict
+    def test_doc_create_pat_appnt(self):
+        pass
+
+    def test_nurse_create_pat_appnt(self):
+        pass
+
+    def test_pat_create_pat_appnt(self):
+        pass
+
+    # update tests
+    def test_doc_update_pat_appnt(self):
+        pass
+
+    def test_nurse_update_pat_appnt(self):
+        pass
+
+    def test_pat_update_pat_appnt(self):
+        pass
+
+    # cancel tests
+    # Note: Nurses cannot cancel appointments
+    def test_doc_cancel_pat_appnt(self):
+        pass
+
+    def test_pat_cancel_pat_appnt(self):
+        pass
+
+    # view tests
+    # Note: Nurses cannot view appointment calendars
+    def test_doc_view_appnt_calendar(self):
+        pass
+
+    def test_pat_view_appnt_calendar(self):
+        pass
+
+
+class PersonalInformation(TestCase):
+    """
+    Update patient personal information
+    """
+    # success case
+    def test_pat_update_info_success(self):
+        pass
+
+    # failure cases
+    def test_pat_update_info_fail_clear_field(self):
+        pass
+
+    def test_pat_update_info_fail_cancel(self):
+        pass
+
+class MedicalInformation(TestCase):
+    """
+    Deals with updating and viewing medical information
+    """
+    # *** Update Medical Information *** #
+    # success cases
+    def test_doc_update_pat_med_info_success(self):
+        pass
+    def test_nurse_update_pat_med_info_success(self):
+        pass
+
+    # failure cases
+    def test_doc_update_pat_med_info_fail_clear_field(self):
+        pass
+
+    def test_doc_update_pat_med_info_cancel(self):
+        pass
+
+    def test_nurse_update_pat_med_info_fail_clear_field(self):
+        pass
+
+    def test_nurse_update_pat_med_info_cancel(self):
+        pass
+
+    # *** View Medical Information *** #
+    # success cases; cannot fail
+    def test_doc_view_pat_med_info_success(self):
+        pass
+
+    def test_nurse_view_pat_med_info_success(self):
+        pass
+
+
+class ActivityLogStats(TestCase):
+    """
+    Admin performs these operations
+    Needs to deal with Success and Failure
+    """
+    # success cases
+    def view_activity_log_success(self):
+        pass
+
+    def view_stats_success(self):
+        pass
+
+    # failure cases
+    def view_activity_log_fail(self):
+        pass
+
+    def view_stats_fail(self):
+        pass
+
+
+class ExportInfo(TestCase):
+    """
+    Needs to deal with Success and Failure
+    """
+    # success case
+    def test_export_info_success(self):
+        pass
+
+    # failure cases
+    def test_export_info_fail_incorrect_sec_qs(self):
+        pass
+
+    def test_export_info_fail_clicks_no(self):
+        pass
+
+    def test_export_info_fail_cancel(self):
+        pass
+
+
+class Prescription(TestCase):
+    """
+    Deals with adding and deleting prescriptions
+    """
+    # *** Adding Prescriptions *** #
+    # success case
+    def test_doc_add_rx_success(self):
+        pass
+
+    # failure cases
+    def test_doc_add_rx_fail_incomplete(self):
+        pass
+
+    def test_doc_add_rx_fail_cancel(self):
+        pass
+
+    # *** Deleting Prescriptions *** #
+    # success case
+    def test_doc_del_rx_success(self):
+        pass
+
+    # failure case
+    def test_doc_del_rx_cancel(self):
+        pass
+
+
+### Need to add admin capable of doing this as well?
+### Test tracker doesn't plan for this though
+class Discharge(TestCase):
+
+    # success case
+    def test_doc_discharge_pat_success(self):
+        pass
+
+    # failure cases
+    def test_doc_discharge_pat_fail_cancel(self):
+        pass
+
+class Admit(TestCase):
+
+    # success cases
+    def test_doc_admit_pat(self):
+        pass
+
+    def test_nurse_admit_pat(self):
+        pass
+
+    # failure cases
+    def test_doc_admit_pat_fail_incorrect(self):
+        pass
+
+    def test_doc_admit_pat_fail_cancel(self):
+        pass
+
+    def test_nurse_admit_pat_fail_incorrect(self):
+        pass
+
+    def test_nurse_admit_pat_fail_cancel(self):
+        pass
+
+
+### Note: pm = Private Message
+### Note: dne = recepient does not exist
+class PrivateMessage(TestCase):
+
+    """
+    Deals with sending and viewing Private Messaging between
+    patients, doctors, nurses, and admins.
+    """
+
+    # *** Send Private Messages *** #
+    # success cases
+    def test_pat_send_pm_success(self):
+        pass
+
+    def test_doc_send_pm_success(self):
+        pass
+
+    def test_nurse_send_pm_success(self):
+        pass
+
+    def test_admin_send_pm_success(self):
+        pass
+
+    # failure cases #
+    # patient fails
+    def test_pat_send_pm_fail_dne(self):
+        pass
+
+    def test_pat_send_pm_fail_incomplete(self):
+        pass
+
+    def test_pat_send_pm_fail_cancel(self):
+        pass
+
+    # doctor fails
+    def test_doc_send_pm_fail_dne(self):
+        pass
+
+    def test_doc_send_pm_fail_incomplete(self):
+        pass
+
+    def test_doc_send_pm_fail_cancel(self):
+        pass
+
+    # nurse fails
+    def test_nurse_send_pm_fail_dne(self):
+        pass
+
+    def test_nurse_send_pm_fail_incomplete(self):
+        pass
+
+    def test_nurse_send_pm_fail_cancel(self):
+        pass
+
+    # admin fails
+    def test_admin_send_pm_fail_dne(self):
+        pass
+
+    def test_admin_send_pm_fail_incomplete(self):
+        pass
+
+    def test_admin_send_pm_fail_cancel(self):
+        pass
+
+    # *** View Private Messages *** #
+    # success cases; no failures for viewing
+    def test_pat_view_pm_success(self):
+        pass
+
+    def test_doc_view_pm_success(self):
+        pass
+
+    def test_nurse_view_pm_success(self):
+        pass
+
+    def test_admin_view_pm_success(self):
+        pass
+
+class MedicalResults(TestCase):
+    # *** Upload *** #
+    # success case
+    def test_doc_upload_med_res_success(self):
+        pass
+
+    # failure cases
+    def test_doc_upload_med_res_fail_incomplete(self):
+        pass
+    def test_doc_upload_med_res_fail_cancel(self):
+        pass
+
+    # *** Release *** #
+    # success case
+    def test_doc_release_med_res__success(self):
+        pass
+
+    # failure case
+    def test_doc_release_med_res_fail_cancel(self):
+        pass
+
+class Transfer(TestCase):
+    # success case
+    def test_doc_transfer_pat_success(self):
+        pass
+    def test_admin_transfer_pat_success(self):
+        pass
+
+    # failure case
+    def test_doc_transfer_pat_fail_same_hosp(self):
+        pass
+    def test_doc_transfer_pat_fail_no_doctor(self):
+        pass
+    def test_admin_transfer_pat_fail_same_hosp(self):
+        pass
+    def test_admin_transfer_pat_fail_no_doctor(self):
+        pass
