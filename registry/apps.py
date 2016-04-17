646d547a90b8f81b386cc6d9ee9e0d81852da282
Matthew Crocco <matthewcrocco@gmail.com>
2016-04-17 16:33:31 -0400

    Add makemigrations and migrate check

--- registry/apps.py
+++ registry/apps.py
@@ -1,11 +1,13 @@
 from django.apps import AppConfig
 from django.db import migrations
+import sys
 
 
 class RegistryConfig(AppConfig):
     name = 'registry'
 
     def ready(self):
-        if not getattr(migrations, 'MIGRATION_OPERATION_IN_PROGRESS', False):
+        if not getattr(migrations, 'MIGRATION_OPERATION_IN_PROGRESS', False) \
+                and 'makemigrations' not in sys.argv and 'migrate' not in sys.argv:
             from registry.utility.parsing import parse_conditions
             parse_conditions('conditions.xml')
