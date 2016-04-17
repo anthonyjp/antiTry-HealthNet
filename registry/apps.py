830ddf3430cbad0064c51c27d57b396d1766afa5
Matthew Crocco <matthewcrocco@gmail.com>
2016-04-17 16:25:52 -0400

    Fix for code running during migrations when it shouldnt

--- registry/apps.py
+++ registry/apps.py
@@ -1,31 +1,11 @@
 from django.apps import AppConfig
-from django.db.models.signals import pre_migrate, post_migrate
-from django.dispatch import receiver
-
-_is_migrating = False
-
-
-@receiver(pre_migrate)
-def migrate_happening(**kwargs):
-    global _is_migrating
-    _is_migrating = True
-
-
-@receiver(post_migrate)
-def migrate_ended(**kwargs):
-    global _is_migrating
-    _is_migrating = False
+from django.db import migrations
 
 
 class RegistryConfig(AppConfig):
     name = 'registry'
 
     def ready(self):
-        if not _is_migrating:
+        if not getattr(migrations, 'MIGRATION_OPERATION_IN_PROGRESS', False):
             from registry.utility.parsing import parse_conditions
             parse_conditions('conditions.xml')
-        else:
-            @receiver(post_migrate)
-            def exec(**kwargs):
-                from registry.utility.parsing import parse_conditions
-                parse_conditions('conditions.xml')
